eb18b5af6b49713d0ad6471bcf1e0437bd6fc65c
Chris Lin <chris.lin@accenturefederal.com>
2017-11-08 09:57:09 -0500

    added publishing

---
+++ src/main/scala/org/mock/addon/s3/AppArgs.scala
@@ -0,0 +1,19 @@
+package org.mock.addon.s3
+
+import org.rogach.scallop.ScallopConf
+
+//class S3MockConf(arguments: Seq[String]) extends ScallopConf(arguments) {
+//  val port = opt[Int](default = Some(7373))
+//  val storage = opt[String](default = Some("/tmp/s3mock"))
+//  val inmemory = opt[Boolean](default = Some(false))
+//  verify()
+//}
+
+object AppArgs {
+  def main(args: Array[String]) {
+//    val conf = new S3MockConf(args)  // Note: This line also works for "object Main extends App"
+//    println("port are: " + conf.port())
+//    println("storage are: " + conf.storage())
+//    println("inmemory are: " + conf.inmemory())
+  }
+}
